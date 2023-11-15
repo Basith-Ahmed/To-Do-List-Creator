e246ffe798ad77f1a2c491a229a6cbb5bbed3c4e
Basith Ahmed N A <143781280+Basith-Ahmed@users.noreply.github.com>
2023-11-15 13:18:31 +0530

    Added taskforge.js

---
+++ taskforge.js
@@ -0,0 +1,38 @@
+const todoList = [];
+
+function keyDown(event) {
+  //enter key func
+  if (event.key === "Enter") {
+    addTodo();
+  }
+}
+
+function addTodo() {
+  //js data array storage
+  const taskElement = document.querySelector(".js-input");
+  const dateElement = document.querySelector(".js-date");
+  const task = taskElement.value;
+  const date = dateElement.value;
+  todoList.push({ task: task, date: date });
+  console.log(todoList);
+  taskElement.value = "";
+  renderTodoList();
+}
+
+function renderTodoList() {
+  //generating html and displaying
+  let todoHTML = "";
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    const task = todoObject.task;
+    const date = todoObject.date;
+    const html = `<div style="padding-left: 10px;">${
+      Number(i) + 1
+    } &#8226; ${task}</div><div style="padding-left: 10px;">${date}</div> <button class="btn btn-secondary" onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList();
+        ">Delete</button>`;
+    todoHTML = todoHTML + html;
+    document.querySelector(".js-todo-array").innerHTML = todoHTML;
+  }
+}
