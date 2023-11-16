21a71571c83c97d17d222d0d048448ddf70fdcfe
Basith Ahmed N A <basithahmeddd@gmail.com>
2023-11-16 16:14:49 +0530

    fixed issue#0001 and Issue#0003

--- taskforge.js
+++ taskforge.js
@@ -1,4 +1,4 @@
-const todoList = [];
+const todoList = JSON.parse(localStorage.getItem("todo")) || [];
 
 function keyDown(event) {
   //enter key func
@@ -15,6 +15,7 @@ function addTodo() {
   const date = dateElement.value;
   todoList.push({ task: task, date: date });
   console.log(todoList);
+  localStorage.setItem("todo", JSON.stringify(todoList));
   taskElement.value = "";
   renderTodoList();
 }
@@ -28,8 +29,14 @@ function renderTodoList() {
     const html = `<div style="padding-left: 10px;">${
       Number(i) + 1
     } &#8226; ${task}</div><div style="padding-left: 10px;">${date}</div> <button class="btn btn-secondary" onclick="
+      console.log(${todoList.length})
+      if (${todoList.length} === 1) {
         todoList.splice(${i}, 1);
-        renderTodoList();
+        location.reload(); 
+      } else {
+        todoList.splice(${i}, 1);
+      }
+      renderTodoList();
         ">Delete</button>`;
     todoHTML = todoHTML + html;
     document.querySelector(".js-todo-array").innerHTML = todoHTML;
